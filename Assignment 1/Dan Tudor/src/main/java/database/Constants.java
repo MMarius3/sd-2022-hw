4ed2b0f1fea114e63f628ab418a45bed77b60450
tudordan25 <tudordan25@gmail.com>
2022-03-13 18:36:22 +0200

    Create project

---
+++ Assignment 1/Dan Tudor/src/main/java/database/Constants.java
@@ -0,0 +1,10 @@
+package database;
+
+public class Constants {
+    public static class Schemas {
+        public static final String TEST = "";
+        public static final String PRODUCTION = "";
+
+        public static final String[] SCHEMAS = new String[]{TEST, PRODUCTION};
+    }
+}
